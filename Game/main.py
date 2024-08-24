45006c03245499c66f3635c5b8d3667b9256fc35
Ted <tvoo001@ucr.edu>
2024-08-24 01:25:19 -0700

    Fixed Login/Register Bug

--- Game/main.py
+++ Game/main.py
@@ -14,6 +14,7 @@ def index():
 def gamemodes():
     return render_template('gamemodes.html')
 
+@main.route('/profile')
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
