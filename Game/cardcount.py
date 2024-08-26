edb5723c8b2f69661bc6b0c10926e21db75db894
ochen011 <91513864+ochen011@users.noreply.github.com>
2024-08-26 11:21:05 -0700

    Update cardcount.py

--- Game/cardcount.py
+++ Game/cardcount.py
@@ -4,12 +4,12 @@ import random
 
 cardcount = Blueprint('cardcount', __name__)
 
-# Card values and Hi-Lo values
 card_values = {
     '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
     'J': 10, 'Q': 10, 'K': 10, 'A': 11
 }
 
+# Card values and Hi-Lo values
 hi_lo_values = {
     '2': 1, '3': 1, '4': 1, '5': 1, '6': 1,
     '7': 0, '8': 0, '9': 0,
